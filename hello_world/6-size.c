05abf65e89c9e199da595c1e733b6c93e63b850d
root <7688@holbertonschool.com>
2023-10-18 16:38:39 -0700

    6-size.c

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -8,11 +8,20 @@
 int main(void)
 
 {
-printf("Size of a char:" %c byte(s)n/");
-printf("Size of an int: " %i bytes(s)n/");
-printf("Size of a long int:" %li byte(s)n/");
-printf("Size of a long long  int:" %lli byte(s)n/");
-pintf("Size of a float:" %f byte(s)n/");
+char c;
+int i;
+long int li;
+long long int lli;
+float f;
+	
+
+	
+	
+printf("Size of a char:" %c byte(s)\n");
+printf("Size of an int: " %i bytes(s)\n");
+printf("Size of a long int:" %li byte(s)\n");
+printf("Size of a long long  int:" %lli byte(s)\n");
+pintf("Size of a float:" %f byte(s)\n");
 return (0);
 
 
