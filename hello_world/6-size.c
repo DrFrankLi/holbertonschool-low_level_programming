024ba097ae8b0dccddb79c6544b9615b106318f2
root <7688@holbertonschool.com>
2023-10-18 16:31:24 -0700

    6-size.c

---
+++ hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/**
+ *  main - Entry point
+ *  Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+printf("Size of a char:" %c byte(s)n/");
+printf("Size of an int: " %i bytes(s)n/");
+printf("Size of a long int:" %li byte(s)n/");
+printf("Size of a long long  int:" %lli byte(s)n/");
+pintf("Size of a float:" %f byte(s)n/");
+return (0);
+
+
+}
