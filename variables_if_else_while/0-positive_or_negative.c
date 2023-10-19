c9258e6a69fe19b04a872c43f0a7ee62c6000372
root <7688@holbertonschool.com>
2023-10-18 21:50:58 -0700

    0-positive_or_negative

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -12,7 +12,7 @@ int main(void)
 	int n;
 
 	srand(time(0));
-	n = rand() - RAND_MAX / 2;	
+	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 		{ printf("%d is positive\n", n);
 			}
@@ -21,6 +21,6 @@ int main(void)
 			}
 	else
 		{ printf("%d is negative\n", n);
-			}		
+			}
 	return (0);
 }
