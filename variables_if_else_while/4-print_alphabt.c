5358c5dabbddf7e48196859ab9a802d62f2f9853
root <7688@holbertonschool.com>
2023-10-19 16:16:16 -0700

    4-print_alphabt.c

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Putchar alphabet in lowercase wihtout e and q followed by \n
+ * Return: Always 0 (Successs)
+ */
+
+int main(void)
+{
+	char n = 'a';
+
+	while (n <= 'z');
+	{
+		if (n != 'q' && n != 'e')
+			putchar(n);
+			n++;
+	}
+	putchar('\n');
+	return (0);
+}
+
+
