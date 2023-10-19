8903939e1889bc557eb9e351857529ecacbead98
root <7688@holbertonschool.com>
2023-10-19 15:37:19 -0700

    2-print_alphabet

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - putchar alphabet in lowercase in lower case
+ * Return - Always 0 (Success)
+ */
+
+int main (void)
+{
+	char n = 'a';
+	while (n <= 'z')
+	{putchar (n);n++;}
+	putchar ('\n');
+	return (0);
+}
+		
