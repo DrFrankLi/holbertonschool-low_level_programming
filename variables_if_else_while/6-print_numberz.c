a7e9d18245ef2d1c873857600389ab49e5f41dd7
root <7688@holbertonschool.com>
2023-10-19 17:21:48 -0700

    6-print_numberz.c

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Putchar single digit smaller than 10 from 0,\n
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n = 0;
+	while (n <= 10)
+	{putchar ('n');
+		n++;}
+
+	putchar ('\n');
+	return (0);
+	}
