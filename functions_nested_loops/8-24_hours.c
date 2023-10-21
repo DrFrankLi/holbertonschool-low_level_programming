e56b3eed5c58748986147c59b3e6c10bd1d3e5c2
root <7688@holbertonschool.com>
2023-10-20 21:29:50 -0700

    8_24_hours

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Function prints minutes of the day of JB 00:00 - 23:59
+ * Return: nothing
+ */
+
+void jack_bauer(void)
+{
+	int h;
+	int m;
+
+	while (h >= 0 && h < 24)
+	{
+		while (m >= 0 && m < 60)
+		{
+			_putchar ('0' + m);
+			m++;
+		}
+		_putchar ('0' + h);
+		h++;
+	}
+}
