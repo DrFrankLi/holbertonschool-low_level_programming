4a305faf6f1630035530a95e602289ec174df276
root <7688@holbertonschool.com>
2023-10-21 16:37:12 -0700

    9-times_table

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table from 0
+ * Return: 0
+ */
+
+void times_table(void)
+{
+	int x;
+	int y;
+
+	while ( y < 10)
+	{
+		while ( x < 10)
+		{
+			_putchar ( x*y + '0');
+			_putchar (',');
+			x++;
+		}
+		y++;
+		_putchar ('\n');
+	}
+}
