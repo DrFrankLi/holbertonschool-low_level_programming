f016160c55545f884a23d81c6b170be34c0090fc
root <7688@holbertonschool.com>
2023-10-20 18:15:01 -0700

    2-print_alphabet.c

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -8,10 +8,16 @@
 void print_alphabet_x10(void)
 {
 	int n = 0;
+	char m = 'a';
 
 	while (n < 10)
 	{
-		_putchar('abcdefghijklmnopqrstuvwxyz');
+		while (m <= 'z')
+		{
+			_putchar(m);
+			m++;
+		}
+		_putchar(n);
 		n++;
 	}
 	_putchar ('\n');
