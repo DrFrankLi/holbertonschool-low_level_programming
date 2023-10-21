b7e267317312bb20e8cb8d30783516c5e371d7e2
root <7688@holbertonschool.com>
2023-10-21 16:15:52 -0700

    8-24_hours

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -11,25 +11,19 @@ void jack_bauer(void)
 	int m;
 
 	h = 0;
-	while (h >= 0 && h < 24)
+	while (h < 24)
 	{
 		m = 0;
-		while (m >= 0 && m < 60)
+		while (m < 60)
 		{
-			if (m >= 0 && m <=9)
-			{
-				_putchar ('0');
-				_putchar (48 + m);
-				m++;
-			}
-			else
-			{
-				_putchar (48 + m);
-				m++;
-			}
-
+			_putchar(h / 10 + '0');
+			_putchar(h % 10 + '0');
+			_putchar(':');
+			_putchar(m / 10 + '0');
+			_putchar(m % 10 + '0');
+			_putchar('\n');
+			m++;
 		}
-		_putchar (48 + h);
 		h++;
 	}
-}
+}	
