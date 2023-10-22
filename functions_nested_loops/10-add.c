23b2803c69d4d6b34ef2744351de6c62cf588bad
root <7688@holbertonschool.com>
2023-10-21 18:55:22 -0700

    10-add

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns result
+ * @a: first integer
+ * @b: another integer
+ * Return: Always 0
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
