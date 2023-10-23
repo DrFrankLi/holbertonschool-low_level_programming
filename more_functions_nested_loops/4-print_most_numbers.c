eea9ce4a2160e6db0bfbee9e3e28fdf6469d96e3
root <7688@holbertonschool.com>
2023-10-23 02:31:26 -0700

    4-print_most_numbers.c

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -8,13 +8,14 @@
 
 void print_most_numbers(void)
 {
-	int n = '0';
+	int n;
 
+	n = '0';
 	while (n <= 9)
 	{
 		if (n != 2 && n != 4)
 		{
-			_putchar(48 + n);
+			_putchar('0' + n);
 			n++;
 		}
 	}
