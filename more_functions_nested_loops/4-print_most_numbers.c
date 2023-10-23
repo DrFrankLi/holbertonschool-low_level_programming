397e3cfd26ccc88137eea8cae78de29265484a36
root <7688@holbertonschool.com>
2023-10-23 00:16:03 -0700

    4-print_most_numbers.c

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"                                                                                 
+#include <stdio.h>                                                                                
+                                                                                                  
+/**                                                                                               
+ * print_most_numbers - prints the number 0-9 with no 2 and 4, \n                                 
+ * Return: Always 0                                                                               
+ */                                                                                               
+                                                                                                  
+void print_most_numbers(void)                                                                     
+{
+	int n = '0';
+
+	while (n <= 9)
+	{
+		if (n != 2 && n!= 4)
+		{
+			_putchar(n);
+			n++;
+		}
+	}
+	_putchar('\n');
+}
