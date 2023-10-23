6696befa674004ce0253578342c1ff89e208238f
root <7688@holbertonschool.com>
2023-10-23 03:03:10 -0700

    5-more_numbers.c

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -11,14 +11,18 @@ void more_numbers(void)
 	char h = '0';
 	char v = '0';
 
-	while (v <= 10)
+	while (v < 10)
 	{
 		while (h <= 14)
 		{
-			_putchar(h);
+			if (h > 9)
+			{
+				_putchar(h / 10);
+			}
+			_putchar(h % 10);
 			h++;
 		}
+		_putchar('\n');
 		v++;
 	}
-	_putchar('\n');
 }
