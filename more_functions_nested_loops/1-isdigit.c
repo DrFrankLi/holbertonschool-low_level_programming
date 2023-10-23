026492d0480d15ea063de213a86593cbd1823c8d
root <7688@holbertonschool.com>
2023-10-22 23:33:38 -0700

    1-isdigit

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -4,14 +4,14 @@
 /**
  * _isdigit - checks for a digit
  * @c: is the int being tested
- * Return: 1 if c is a digit 
+ * Return: 1 if c is a digit
  */
 
 int _isdigit(int c)
 {
-	if (c >=0 && c <= 9)
+	if (c >= 0 && c <= 9)
 	{
-		return (1);
+		return ('1');
 	}
 	else
 	{
