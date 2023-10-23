2c6ff8bc92b2e174a2d6edd94bf0beb5344962a4
root <7688@holbertonschool.com>
2023-10-23 05:00:01 -0700

    9-fizz_buzz.c

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+/**
+ * main - print numbers 1-100 fizz for 3* buzz for 5* fizzbuzz for 15*
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int n = 0;
+
+	if (n % 3 == 0)
+	{
+		putchar('F');
+		putchar('i');
+		putchar('z');
+		putchar('z');
+		putchar(' ');
+	}
+	else if (n % 5 == 0)
+	{
+		putchar('B');
+		putchar('u');
+		putchar('z');
+		putchar('z');
+		putchar(' ');
+	}
+	else if (n % 15 == 0)
+	{
+		putchar('F');
+		putchar('i');
+		putchar('z'); 
+		putchar('z'); 
+		putchar('B');
+		putchar('u');
+		putchar('z'); 
+		putchar('z'); 
+		putchar(' ');
+	}	
+	else
+	{
+		putchar('0' + n);
+		putchar(' ');
+	}
+	return (0);
+}
