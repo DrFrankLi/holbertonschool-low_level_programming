0a101dcbab4696cc5b96f984fae88286fcfefc7b
root <7688@holbertonschool.com>
2023-10-26 17:35:31 -0700

    4-print_rev.c

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -8,14 +8,14 @@
 
 void print_rev(char *s)
 {
-	int length = 0;
-	int i = 0;
+	int length;
+	int i;
 
-	while (s[length] >= 0)
+	length = 0;
+	while (s[length] != '\0')
 	{
 		length++;
 	}
-	
 	i = length - 1;
 	while (i >= 0)
 	{
