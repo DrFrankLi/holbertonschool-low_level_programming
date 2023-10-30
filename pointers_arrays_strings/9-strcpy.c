e5b3b319934a7d5d512e7ea289dcdc9734bea896
root <7688@holbertonschool.com>
2023-10-30 02:42:01 -0700

    9-strcpy.c

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcpy - copies the tring pointed to by src with null to finish
+ * @dest: destition pointer char
+ * @src: source pointer char
+ * Return: Always 0
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	char *original_dest = dest;
+
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return original_dest;
+}
