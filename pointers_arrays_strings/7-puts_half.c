9f496061cd90fddf20214b2f9f20f7b14eaf1195
root <7688@holbertonschool.com>
2023-10-29 17:18:55 -0700

    7-puts_half.c

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -9,16 +9,17 @@
 void puts_half(char *str)
 {
 	int len = 0;
-	int i = len / 2;
+	int i = 0;
 
 	while (str[len] != '\0')
 	{
-		if (i >= (len / 2))
-		{
-			_putchar(str[i]);
-			i++;
-		}
 		len++;
+	}
+	i = (len + 1) / 2;
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
 	}
 	_putchar('\n');
 }
