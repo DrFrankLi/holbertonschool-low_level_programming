3521066c942dc9f395f98b8861d45bf9a142a867
root <7688@holbertonschool.com>
2023-10-29 21:01:55 -0700

    8-print_array.c

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -10,12 +10,13 @@
 
 void print_array(int *a, int n)
 {
+	int i = 0;
 	n = 0;
 
-	while (n >= 0)
+	while (i < n)
 	{
-		printf("%d, ", a[n]);
-		n++;
+		printf("%d, ", a[i]);
+		i++;
 	}
 	return;
 }
