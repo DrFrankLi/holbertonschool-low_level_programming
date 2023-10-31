ad7ebdf8d14c2bc6f113a71781c3af12089bc35c
root <7688@holbertonschool.com>
2023-10-31 01:13:54 -0700

    2-strncpy.c

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strncpy - copies a string
+ * @dest: destination char pointer
+ * @src: source char pointer
+ * @n: integer number of array copied from src
+ * Return: Always 0
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int len_d = 0;
+	int len_s = 0;
+
+	while (len_s != '\0')
+	{
+		if (len_s <= n)
+		{
+			dest[len_d] = src[len_s];
+			len_d++;
+			len_s++;
+		}
+		else if (src[n] == '\0')
+		{
+			dest[len_d] = src[n];
+			len_d++;
+			n++;
+		}
+	}
+	return (dest);
+}
