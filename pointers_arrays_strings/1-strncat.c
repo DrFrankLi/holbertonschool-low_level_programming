22c05b86bdf8c51dc4d8b2386d528a71b15a64b4
root <7688@holbertonschool.com>
2023-10-31 00:25:40 -0700

    1-strncat.c

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -14,24 +14,30 @@ char *_strncat(char *dest, char *src, int n)
 	int len_d;
 	int len_s;
 
+	len_d = 0;
 	while (dest[len_d] != '\0')
 	{
 		len_d++;
 	}
+	len_s = 0;
 	while (src[len_s] != '\0')
 	{
 		len_s++;
 	}
-	if (len_s <= n)
+	while (n > 0)
 	{
-		dest[len_d] = src[len_s];
-		len_d++;
-		len_s++;
-	}
-	else
-	{
-		dest[len_d] = src[n];
-		len_d++;
+		if (len_s <= n)
+		{
+			dest[len_d] = src[len_s];
+			len_d++;
+			len_s++;
+		}
+		else
+		{
+			dest[len_d] = src[n];
+			len_d++;
+			n++;
+		}
 		n++;
 	}
 	return (dest);
