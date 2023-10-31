0ad2e5b7a4986d10d85e1751e7e72c63741aa4c8
root <7688@holbertonschool.com>
2023-10-30 17:22:57 -0700

    100-atoi.c

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -20,7 +20,7 @@ int _atoi(char *s)
 		if (s[i] == '-' && result == 0)
 		{
 			sign = sign * (-1);
-		}	
+		}
 		else if (s[i] >= '0' && s[i] <= '9')
 		{
 			result = (result * 10) + (s[i] - '0');
