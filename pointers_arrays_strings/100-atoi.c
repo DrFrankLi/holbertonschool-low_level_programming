b213501d1bfd5bb15142fa46ee32f1928403768d
root <7688@holbertonschool.com>
2023-10-30 17:05:38 -0700

    100-atoi.c

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -17,17 +17,17 @@ int _atoi(char *s)
 		return (0);
 	while (s[i] != '\0')
 	{
-		if (s[i] >= '0' || s[i] <= '9')
+		if (s[i] == '-')
 		{
-			result = result * 10 + (s[i] + '0');
+			sign = sign * (-1);
+		}	
+		else if (s[i] >= '0' || s[i] <= '9')
+		{
+			result = result * 10 + (s[i] - '0');
 		}
 		else if (result > 0)
 		{
 			break;
-		}
-		else if (s[i] == '-')
-		{
-			sign = sign * (-1);
 		}
 		i++;
 	}
