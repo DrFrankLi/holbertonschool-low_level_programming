1f780b881f05d93d63afbcbf4535df9c744f2b0d
root <7688@holbertonschool.com>
2023-10-30 21:13:36 -0700

    0-stracat.c

---
+++ pointers_arrays_strings/0-stracat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _stracat - concatenates two strings
+ * @dest: destination 
+ * @src: source
+ * Return: Always 0
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int len_d = 0;
+	int len_s = 0;
+
+	while (dest[len_d] != '\0')
+	{
+		len_d++;
+	}
+	while (src[len_s] != '\0')
+	{
+		dest[len_d] = src[len_s];
+		len_s++;
+		len_d++;
+	}
+	dest[len_d] = '\0';
+	return (dest);
+}
