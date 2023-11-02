15151fa3e173f6c130c6cfc3d25da3f8fb8f7024
root <7688@holbertonschool.com>
2023-11-01 21:43:03 -0700

    3-strcmp.c

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -13,24 +13,14 @@ int _strcmp(char *s1, char *s2)
 	int i = 0;
 	int j = 0;
 
-	while (*s1 != 0 && *s2 != 0)
+	while (s1[i] != 0 && s2[j] != 0)
 	{
-		i = i * 10 + (*s1 - '\0');
-		j = j * 10 + (*s2 - '\0');
+		if (s1[i] != s2[j])
+		{
+			return (s1[i] - s2[j]);
+		}
 		i++;
 		j++;
-	}
-	if (i > j)
-	{
-		printf("-%d\n", i);
-	}
-	else if (i < j)
-	{
-		printf("%d\n", i);
-	}
-	else
-	{
-		printf("0");
 	}
 	return (0);
 }
