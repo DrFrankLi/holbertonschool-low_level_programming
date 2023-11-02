cbd12a942d89cb1f7559a418eabcece267780b3d
root <7688@holbertonschool.com>
2023-11-02 02:49:28 -0700

    4-rev_array.c

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * reverse_array: reverses the content of an array of integers
+ * reverse_array - reverses the content of an array of integers
  * @a: one integer string pointer
  * @n: source string that needs to be reserved
  * Return: always 0
@@ -13,7 +13,7 @@ void reverse_array(int *a, int n)
 
 	while (a[i] != '\0' && i <= n)
 	{
-		a[i] = a[n-1];
+		a[i] = a[n - 1];
 		i++;
 		n--;
 	}
