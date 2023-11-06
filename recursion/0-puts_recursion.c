2bde56976ce6d1b9cd96d022fc9f8d1711bab6b6
root <7688@holbertonschool.com>
2023-11-05 18:06:27 -0800

    0-puts_recursion.c

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -9,11 +9,11 @@
 
 void _puts_recursion(char *s)
 {
-	int i = 0;
-
-	while (i >= 0)
+	if (*s == '\0')
 	{
-		_putchar(s[i]);
-		i++;
+		putchar('\n');
+		return;
 	}
+	putchar(*s);
+	_puts_recursion(s + 1);
 }
