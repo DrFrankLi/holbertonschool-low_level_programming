4f6d2a79d5760c173c849dabd383001eed0714fb
root <7688@holbertonschool.com>
2023-11-05 19:16:52 -0800

    4-pow_recursion.c

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -14,5 +14,12 @@ int _pow_recursion(int x, int y)
 	{
 		return (-1);
 	}
-	return (_pow_recursion(x, y));
+	else if (y == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
 }
