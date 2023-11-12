ec686b02f792904fbdc38f7a5f47bb446f45021d
root <7688@holbertonschool.com>
2023-11-11 18:00:38 -0800

    1-string_nconcat.c

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -18,11 +18,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int j = 0;
 	char *result;
 
-	if (s1 == NULL || s2 == NULL)
-	{
+	if (s1 == NULL)
 		s1 = "";
+	if (s2 == NULL)
 		s2 = "";
-	}
 	while (s1[len1] != '\0')
 	{
 		len1++;
