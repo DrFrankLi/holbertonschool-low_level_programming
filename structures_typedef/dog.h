68e544243bdf45c3882cbc82af66815fa3647d15
root <7688@holbertonschool.com>
2023-11-12 19:58:02 -0800

    dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,14 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - brief represnets info about a dog
+ * @name: dog name
+ * @age: dog age
+ * @owner :dog owner
+ * This sturcture includes dog details name, age, owner
+ */
+
 struct dog
 {
 	char *name;
